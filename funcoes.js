db2b26e1bb05fcbe9281594cc42e141ea45fa2fc
Michel Fernandes <michelbong1@hotmail.com>
2021-03-22 08:33:20 -0300

    tabela criada

--- funcoes.js
+++ funcoes.js
@@ -1,8 +1,10 @@
-function enviar_formulario(formulario){
 
-    document.getElementById("result-form").innerHTML = formulario.nome.value
+function enviar_formulario() {
+    var linha = document.createElement('tr');
+    linha.insertCell(0).innerHTML += document.getElementById('nome').value;
+    linha.insertCell(1).innerHTML += document.getElementById('email').value; 
+    linha.insertCell(2).innerHTML += document.getElementById('telefone').value;
+    document.getElementById('tabela').appendChild(linha);
 
     return false;
-
-
 }
